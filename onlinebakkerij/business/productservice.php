c7a42cad2538c9c4a20ad8c58c438a99196c14a6
eigenaar <eigenaar@192.168.0.247>
2014-08-14 14:50:19 +0200

    gebruikerservice en DAO gemaakt, productservice en soortservice afgewerkt.

--- onlinebakkerij/business/productservice.php
+++ onlinebakkerij/business/productservice.php
@@ -1,8 +1,38 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("data/productdao.php");
 
+class productservice {
+
+    public function getproductenOverzicht() {
+        $ProductDAO = new ProductDAO();
+        $lijst = $ProductDAO->getAll();
+        return $lijst;
+    }
+
+    public function voegNieuwProductToe($naam, $soortId) {
+        $ProductDAO = new ProductDAO();
+        $ProductDAO->create($naam, $soortId);
+    }
+
+    public function verwijderProduct($id) {
+        $ProductDAO = new ProductDAO();
+        $ProductDAO->delete($id);
+    }
+
+    public function haalProductOp($id) {
+        $ProductDAO = new ProductDAO();
+        $product = $ProductDAO->getById($id);
+        return $product;
+    }
+
+    public function updateProduct($id, $naam, $soortId) {
+        $SoortDAO = new SoortDAO();
+        $ProductDAO = new ProductDAO();
+        $soort = $soortDAO->getById($soortId);
+        $product = $productDAO->getById($id);
+        $product->setNaam($naam);
+        $product->setGenre($soort);
+        $productDAO->update($product);
+    }
+}
--- onlinebakkerij/business/soortservice.php
+++ onlinebakkerij/business/soortservice.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("data/soortdao.php");
+
+class Soortenservice {
+
+    public function getSoortenOverzicht() {
+        $SoortDAO = new SoortDAO();
+        $lijst = $soortDAO->getAll();
+        return $lijst;
+    }
+
+}
 
