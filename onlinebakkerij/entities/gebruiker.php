9c218c630c8ef6b725f37f26517882085f2c8af7
abbyshen <niels.vroman@hotmail.com>
2014-08-20 10:00:08 +0200

    gebruiker is aangepast

--- onlinebakkerij/entities/gebruiker.php
+++ onlinebakkerij/entities/gebruiker.php
@@ -4,22 +4,35 @@ class gebruiker {
 
     private static $idMap = array();
     private $id;
-    private $gebruiker;
+    private $naam;
+    private $voornaam;
     private $wachtwoord;
     private $telefoonnummer;
     private $emailadres;
+    private $woonplaats;
+    private $postcode;
+    private $straat;
+    private $nummer;
 
-    private function __construct($id, $gebruiker, $wachtwoord, $telefoonnummer, $emailadres) {
+    private function __construct($id, $naam, $voornaam, $wachtwoord, $telefoonnummer, $emailadres
+                                ,$woonplaats, $postcode, $straat, $nummer) {
         $this->id = $id;
-        $this->gebruiker = $gebruiker;
+        $this->naam = $naam;
+        $this->voornaam = $voornaam;
         $this->wachtwoord = $wachtwoord;
         $this->telefoonnummer = $telefoonnummer;
         $this->emailadres = $emailadres;
+        $this->woonplaats = $woonplaats;
+        $this->postcode = $postcode;
+        $this->straat = $straat;
+        $this->nummer = $nummer;
     }
 
-    public static function create($id, $gebruiker, $wachtwoord, $telefoonnummer, $emailadres) {
+    public static function create($id, $naam,$voornaam, $wachtwoord, $telefoonnummer, $emailadres
+                                  , $woonplaats, $postcode, $straat, $nummer) {
         if (!isset(self::$idMap[$id])) {
-            self::$idMap[$id] = new gebruiker($id, $gebruiker, $wachtwoord, $telefoonnummer,$emailadres);
+            self::$idMap[$id] = new gebruiker($id, $naam, $voornaam, $wachtwoord, $telefoonnummer
+                                             ,$emailadres,$woonplaats,$postcode,$straat,$nummer);
         }
         return self::$idMap[$id];
     }
@@ -28,8 +41,12 @@ class gebruiker {
         return $this->id;
     }
 
-    public function getGebruiker() {
-        return $this->gebruiker;
+    public function getNaam() {
+        return $this->naam;
+    }
+    
+    public function getVroonaam(){
+        return $this->voornaam;
     }
 
     public function getWachtwoord() {
@@ -44,8 +61,28 @@ class gebruiker {
         return $this->emailadres;
     }
 
-    public function setGebruiker($gebruiker) {
-        $this->gebruiker = $gebruiker;
+    public function getWoonplaats(){
+        return $this->woonplaats;
+    }
+
+    public function getPostcode(){
+        return $this->postcode;
+    }
+    
+    public function getStraat() {
+        return $this->straat;
+    }
+
+    public function getNummer(){
+        return $this->nummer;
+    }
+
+    public function setNaam($naam) {
+        $this->naam = $naam;
+    }
+    
+    public function setVoornaam($voornaam){
+        $this->voornaam = $voornaam;
     }
 
     public function setWachtwoord($wachtwoord) {
@@ -58,5 +95,21 @@ class gebruiker {
 
     public function setEmailadres($emailadres){
         $this->emailadres = $emailadres;
+    }
+    
+    public function setWoonplaats($woonplaats){
+        $this->woonplaats = $woonplaats;
+    }
+    
+    public function setPostcode($postcode){
+        $this->postcode = $postcode;
+    }
+    
+    public function setStraat($straat){
+        $this->straat = $straat;
+    }
+    
+    public function setNummer($nummer){
+        $this->nummer = $nummer;
     }
 }
