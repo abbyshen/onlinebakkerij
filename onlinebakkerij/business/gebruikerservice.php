f4d8a4a1042f9afc97669d02f6a79a08ddbb8417
abbyshen <niels.vroman@hotmail.com>
2014-08-20 13:27:21 +0200

    gebruikersDAO ingevuld

--- onlinebakkerij/business/gebruikerservice.php
+++ onlinebakkerij/business/gebruikerservice.php
@@ -1,8 +1,34 @@
 <?php
+require_once ("entities/gebruiker.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class gebruikerservice {
+    public function voegNieuwGebruikerToe($naam, $voornaam, $wachtwoord, $telefoonnummer
+                                        ,$emailadres, $woonplaats, $postcode, $straat, $nummer) {
+        $gebruikerDAO = new gebruikerDAO();
+        $gebruikerDAO->create($naam, $voornaam,$wachtwoord,$telefoonnummer,$emailadres,$woonplaats
+                                ,$postcode,$straat,$nummer);
+    }
+
+    public function verwijderProduct($id) {
+        $ProductDAO = new ProductDAO();
+        $ProductDAO->delete($id);
+    }
+
+    public function haalProductOp($id) {
+        $ProductDAO = new ProductDAO();
+        $product = $ProductDAO->getById($id);
+        return $product;
+    }
+
+    public function updateProduct($id, $naam,$prijs, $soortId) {
+        $SoortDAO = new SoortDAO();
+        $ProductDAO = new ProductDAO();
+        $soort = $soortDAO->getById($soortId);
+        $product = $productDAO->getById($id);
+        $product->setNaam($naam);
+        $product->setPrijs($prijs);
+        $product->setGenre($soort);
+        $productDAO->update($product);
+    }
+}
 
--- onlinebakkerij/data/gebruikerDAO.php
+++ onlinebakkerij/data/gebruikerDAO.php
@@ -1,8 +1,83 @@
 <?php
+require_once("data/dbconfig.php");
+require_once("entities/gebruiker.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class gebruikerDAO{
+    public function getById($id) {
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $sql = "select id, naam, voornaam, wachtwoord, telefoon, emailadres, woonplaats, postcode
+                , straat and nummer from gebruiker where
+                gebruiker.id = " . $id;
+        $resultSet = $dbh->query($sql);
+        $rij = $resultSet->fetch();
+        $gebruiker = product::create($rij["id"], $rij["naam"], $rij["voornaam"], $rij["wachtwoord"]
+                    ,$rij["telefoonnummer"], $rij["emailadres"], $rij["woonplaats"], $rij["postcode"]
+                    ,$rij["straat"],$rij["nummer"],$rij["geblokkeerd"]);
+        $dbh = null;
+        return $gebruiker;
+    }
+
+    public function getByemailadres($emailadres) {
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $sql = "select id, naam, voornaam, wachtwoord, telefoon, emailadres, woonplaats, postcode
+                , straat and nummer from gebruiker where
+                gebruiker.emailadres = " . $emailadres;
+        $resultSet = $dbh->query($sql);
+        $rij = $resultSet->fetch();
+        if (!$rij) {
+            return null;
+        } else {
+            $gebruiker = product::create($rij["id"], $rij["naam"], $rij["voornaam"], $rij["wachtwoord"]
+                    ,$rij["telefoonnummer"], $rij["emailadres"], $rij["woonplaats"], $rij["postcode"]
+                    ,$rij["straat"],$rij["nummer"],$rij["geblokkeerd"]);
+            $dbh = null;
+            return $boek;
+        }
+    }
+
+    public function create($naam, $voornaam, $wachtwoord, $telefoonnummer, $emailadres
+                                ,$woonplaats, $postcode, $straat, $nummer) {
+        $bestaandemailadres = $this->getByemailadres($emailadres);
+        if (isset($$bestaandemailadres))
+            throw new EmailadresBestaatException();
+        $sql = "insert into product (naam, voornaam, wachtwoord, telefoonnummer, emailadres
+                                ,woonplaats, postcode, straat, nummer)
+                values (" . $naam . ", " . $voornaam . ",". $wachtwoord .",". $telefoonnummer.","
+                . $emailadres .",".$woonplaats.",".$postcode.",".$straat.",".$nummer.")";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $dbh->exec($sql);
+        $gebruikerId = $dbh->lastInsertId();
+        $dbh = null;
+        $gebruiker = gebruiker::create($naam, $voornaam, $wachtwoord, $telefoonnummer, $emailadres
+                                ,$woonplaats, $postcode, $straat, $nummer);
+        return $gebruiker;
+    }
+
+    public function delete($id) {
+        $sql = "delete from gebruiker where id = " . $id;
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $dbh->exec($sql);
+        $dbh = null;
+    }
+
+    public function update($gebruiker) {
+        $bestaandgebruiker = $this->getByemailadres($gebruiker->getEmailadres());
+        if (isset($bestaandgebruiker) && $bestaandgebruiker->getId() != $gebruiker->getId())
+            throw
+            new EmailadresBestaatException();
+        $sql = "update gebruiker set naam='" . $gebruiker->getNaam() .
+                "', voornaam='" . $gebruiker->getVoornaam() .
+                "', wachtwoord='" . $gebruiker->getWachtwoord() .
+                "', telefoonnummer='" . $gebruiker->getTelefoonnummer() .
+                "', emailadres='" . $gebruiker->getEmailadres() .
+                "', woonplaats='" . $gebruiker->getWoonplaats() .
+                "', postcode='" . $gebruiker->getPostcode() .
+                "', straat='" . $gebruiker->getStraat() .
+                "', nummer'" . $gebruiker->getNummer() .
+                "' where id = " . $gebruiker->getId();
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $dbh->exec($sql);
+        $dbh = null;
+    }
+}
 
