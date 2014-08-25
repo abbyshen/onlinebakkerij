2fabe32bacd9ee93502094983fcbe7040f0050e0
abbyshen <niels.vroman@hotmail.com>
2014-08-25 15:58:26 +0200

    navbar

--- onlinebakkerij/presentation/gebruikeraanmelden.php
+++ onlinebakkerij/presentation/gebruikeraanmelden.php
@@ -3,8 +3,50 @@
     <head>
         <meta charset=utf-8>
         <title>aanmelden</title>
+        <style>
+            #hoofdmenu {
+                list-style: none;
+                font-size: 0.875em; /* 14px = 1em */
+                font-weight: bold;
+                padding: 0; /* 0.5em = 6px */
+                margin: 0 1em;
+                width:auto;
+            }
+            #hoofdmenu li:nth-child(1n+0) {
+                width: 16.4%;
+                padding:0.3em 0;
+                border-top: 1px solid LavenderBlush;
+                border-right: 1px solid LavenderBlush;
+                border-bottom: none;
+                border-left: none;
+                text-align: center;
+            }
+            #hoofdmenu li:first-child {
+                border-left:1px solid LavenderBlush;
+            }
+            #hoofdmenu a {
+                display: inline;
+                cursor: pointer;
+            }
+            #hoofdmenu li:hover {
+                color:white;
+                background-color:darkseagreen;
+            }
+        </style>
     </head>
     <body>
+        <header>
+            <div class="container">
+                <h1><a href="home.php" id="logo">Bakkerij vroman</a></h1>
+                <nav id="kopnav">
+                    <ul id="hoofdmenu">
+                        <li><a href="home.php">home</a></li>
+                        <li><a href="assortiment.php">ons assortiment</a></li>
+                        <li><a href="overons.php">over ons</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
         <h1>nieuwe gebruiker toevoegen</h1>
         <?php
         if ($error == "emailexists") {
--- onlinebakkerij/presentation/gebruikerinloggen.php
+++ onlinebakkerij/presentation/gebruikerinloggen.php
@@ -16,8 +16,50 @@ if($gebruikersvc->logincheck()==true){
     <head>
         <meta charset=utf-8>
         <title>inloggen</title>
+        <style>
+            #hoofdmenu {
+                list-style: none;
+                font-size: 0.875em; /* 14px = 1em */
+                font-weight: bold;
+                padding: 0; /* 0.5em = 6px */
+                margin: 0 1em;
+                width:auto;
+            }
+            #hoofdmenu li:nth-child(1n+0) {
+                width: 16.4%;
+                padding:0.3em 0;
+                border-top: 1px solid LavenderBlush;
+                border-right: 1px solid LavenderBlush;
+                border-bottom: none;
+                border-left: none;
+                text-align: center;
+            }
+            #hoofdmenu li:first-child {
+                border-left:1px solid LavenderBlush;
+            }
+            #hoofdmenu a {
+                display: inline;
+                cursor: pointer;
+            }
+            #hoofdmenu li:hover {
+                color:white;
+                background-color:darkseagreen;
+            }
+        </style>
     </head>
     <body>
+        <header>
+            <div class="container">
+                <h1><a href="home.php" id="logo">Bakkerij vroman</a></h1>
+                <nav id="kopnav">
+                    <ul id="hoofdmenu">
+                        <li><a href="home.php">home</a></li>
+                        <li><a href="assortiment.php">ons assortiment</a></li>
+                        <li><a href="overons.php">over ons</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
         <h1>inloggen</h1>
         <form method="post" action="process_login.php?action=inloggen">
             <table>
